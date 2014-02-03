be076b0a2b07434e72e96b503aaa564d34506e1a
Simon Lischka <simon@a-studios.org>
2014-02-03 23:08:30 +0100

    Break down block classes

--- js/Block.js
+++ js/Block.js
@@ -26,10 +26,22 @@
             self.addEventListener('tick', function() {
                 self.tick();
             });
-            instance.addEventListener('stopAllPlayers', function() {
-                self.handleStopAllPlayers();
-            });
         })(this);
+    }
+    p.generateId = function() {
+
+    }
+    p.getId() = function() {
+
+    }
+    p.setSlot() = function() {
+
+    }
+    p.setPlayStatus() = function() {
+
+    }
+    p.getPlayStatus() = function() {
+
     }
     p.handleClick = function() {
         if (this.isPlaying) {
@@ -41,11 +53,6 @@
     p.play = function () {
         this.isPlaying = true;
         instance.setPosition(this.position);
-        this.dispatchEvent('stopAllPlayers');
-        if (instance.playing != true) {
-            instance.play();
-            instance.playing = true;
-        }
     }
     p.pause = function() {
         instance.playing = false;
@@ -58,19 +65,19 @@
         return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
     }
     p.makeShape = function()  {
+        var offset = 50;
+        var mul = 1;
         g = this;
         g.graphics.clear();
+        g.graphics.setStrokeStyle(15);
         for (var i = 0; i < this.circleCount-2; i++) {
-            g.graphics.setStrokeStyle(15);
-            var offset = 50,
-                mul = 1,
-                c_r = Math.round(soundData[i] * 0.5),
-                c_g = Math.round(soundData[i+1] * 0.5),
-                c_b = Math.round(soundData[i+2] * 0.5);
+            var c_r = Math.round(soundData[i] * 0.5),
+            var c_g = Math.round(soundData[i+1] * 0.5),
+            var c_b = Math.round(soundData[i+2] * 0.5);
             var stroke = createjs.Graphics.getRGB(c_r, c_g, c_b);
             g.graphics.beginFill('#000000').beginStroke(stroke);
-            g.graphics.drawCircle(0,0,(i+1)*4);
-            g.alpha = 1-i*0.02;
+            g.graphics.drawCircle(0, 0, (i + 1) * 4);
+            g.alpha = 1 - i * 0.02;
             g.compositeOperation = "lighter";
         }
     }
